6128d16e23a0545c24a0e0576be6bdca284b9ab6
Arnaud MICHOUD <arnaud.michoud@gmail.com>
2024-06-17 22:59:56 +0200

    Application boiler plate

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span>{{ products.length }} produits à votre disposition pour construire votre site e-commerce</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted() {
+    axios.get('https://dummyjson.com/products')
+    .then(result => {
+      this.products = result.data.products;
+    });
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
