186b9df9547dba31fca5ce2579ab115ce268912d
YounessFTN <youness.fatine1@gmail.com>
2024-06-19 22:58:46 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
-    <span
-      >{{ products.length }} produits à votre disposition pour construire votre
-      site e-commerce</span
-    >
+    <h1>
+      Zalanda
+      <img class="logo" src="./assets/Logo_Zalando.svg" alt="Zalando Logo" />
+    </h1>
+    <section class="product-grid">
+      <div class="product" v-for="product in products" :key="product.id">
+        <h2 class="product-title">{{ product.title }}</h2>
+        <img
+          class="product-image"
+          :src="product.thumbnail"
+          :alt="product.title"
+        />
+        <p class="product-description">{{ product.description }}</p>
+        <span>{{ product.price }}</span>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -26,4 +38,64 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+h1 {
+  text-align: center;
+  margin-bottom: 30px;
+  margin-top: 30px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+}
+h1 img {
+  margin-left: 10px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.product-grid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 20px;
+}
+
+.product {
+  width: 300px;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: #8a2be2 0px 0px 3px;
+  border-radius: 30px;
+}
+.product-title {
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+}
+
+.product-image {
+  width: 100%;
+  height: auto;
+  object-fit: contain;
+  margin-bottom: 10px;
+}
+
+.product-description {
+  font-size: 0.9rem;
+  color: #666;
+  text-align: center;
+}
+.logo {
+  width: 50px;
+  height: auto;
+}
+.product span {
+  font-weight: 600;
+  font-size: 30px;
+  color: blueviolet;
+}
+</style>
