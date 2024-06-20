96848fb7712267986c9ff4cc0af96f4f374b82d0
beyegb1n <emmanuel.kouassi@next-u.fr>
2024-06-20 04:24:56 +0200

    Affichage des produits

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
-    <span
-      >{{ products.length }} produits à votre disposition pour construire votre
-      site e-commerce</span
-    >
+    <span class="texte-centre">Liste de produits</span>
+    <div class="liste-des-produits">
+      <div class="produit" v-for="product in products" :key="product.id">
+        <h3>{{ product.title }}</h3>
+        <img :src=product.thumbnail alt=product.title>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -26,4 +30,46 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  background-color: #F5F5F7;
+}
+
+.texte-centre {
+    display: block;
+    text-align: center;
+    margin: 20px 0;
+    font-size: 3em;
+    font-family: 'Helvetica Neue', sans-serif;
+}
+
+.liste-des-produits {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+}
+
+.produit {
+    border: 1px solid #e0e0e0;
+    background-color: #fff;
+    border-radius: 8px;
+    padding: 20px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    width: 20%;
+    transition: box-shadow 0.3s ease;
+    font-family: Helvetica, sans-serif;
+    transition: box-shadow 0.3s ease;
+}
+
+.produit:hover {
+  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+}
+
+
+</style>
